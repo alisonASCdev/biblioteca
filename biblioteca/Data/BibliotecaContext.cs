db45d0b619284d6804e6ccf394e6a6e0525921f2
ALISON SOUSA COSTA <alison.scosta2@senacsp.edu.br>
2023-07-28 17:23:30 -0300

    adicionando funcionalidades na minha biblioteca//

---
+++ biblioteca/Data/BibliotecaContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography.X509Certificates;
+
+namespace biblioteca.data
+{
+    public class BibliotecaContext: DbContext
+    {
+        public BibliotecaContext(DbContextOptions<BibliotecaContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ biblioteca/Models/Publisher.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace biblioteca.Models
+{
+    [Table("Publishers")]
+    public class Publisher
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Column("PublisherName")]
+        public string? Name { get; set; }
+        [Required]
+        [StringLength(50)]
+        [MaxLength(80), MinLength(4)]
+        public string? Site { get; set; }
+
+
+    }
+}
